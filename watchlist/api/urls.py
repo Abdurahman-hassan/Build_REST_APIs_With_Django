8be26823ce939d8d246dfc2480570e166844587c
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2023-03-01 23:31:02 +0200

    Update the urls file

--- watchlist/api/urls.py
+++ watchlist/api/urls.py
@@ -1,13 +1,12 @@
 """URLs for the watchlist app."""
 from django.urls import path
 
-from watchlist.views import (
-    movie_list, movie_detail, single_movie
+from watchlist.api.views import (
+    movie_list, movie_detail
 )
 
 app_name = 'watchlist'
 urlpatterns = [
     path('list/', movie_list, name='movie_list'),
-    path('list/<int:movie_id>', single_movie, name='single_movie'),
     path('<int:movie_id>', movie_detail, name='movie_detail'),
 ]
