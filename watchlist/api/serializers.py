0d7fa1dcb64a1d4113f367d1ea46d178df491a6a
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2023-03-03 01:51:36 +0200

    Add ModelSerializers instead of normal serializers, the serializers has been commented and the validators has been added

--- watchlist/api/serializers.py
+++ watchlist/api/serializers.py
@@ -4,38 +4,20 @@ from rest_framework import serializers
 from watchlist.models import Movie
 
 
-# other types of validators
-def title_unique(value):
-    if Movie.objects.filter(title=value).exists():
-        raise serializers.ValidationError('Title must be unique')
-    return value
+# we can use ModelSerializer to create a serializer
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        # fields = '__all__'
+        exclude = ('id',)
 
-
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=200, validators=[title_unique]
-                                  )  # validators are used to check if the title is unique
-    description = serializers.CharField()
-    active = serializers.BooleanField(default=False)
-    year = serializers.IntegerField()
-
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        # instance.title is the old title
-        # validated_data.get('title', instance.title) is the new title
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.year = validated_data.get('year', instance.year)
-        instance.save()
-        return instance
-
-    # field level validation
     def validate_title(self, value):
         if len(value) < 2:
             raise serializers.ValidationError('Title is too short')
+
+        if Movie.objects.filter(title=value).exists():
+            raise serializers.ValidationError("Title already exists")
+
         return value
 
     # object level validation
@@ -43,3 +25,45 @@ class MovieSerializer(serializers.Serializer):
         if data['title'] == data['description']:
             raise serializers.ValidationError('Title and description must be different')
         return data
+
+
+# we can use serializers.Serializer to create a serializer
+# # other types of validators
+# def title_unique(value):
+#     if Movie.objects.filter(title=value).exists():
+#         raise serializers.ValidationError('Title must be unique')
+#     return value
+#
+#
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(max_length=200, validators=[title_unique]
+#                                   )  # validators are used to check if the title is unique
+#     description = serializers.CharField()
+#     active = serializers.BooleanField(default=False)
+#     year = serializers.IntegerField()
+#
+#     def create(self, validated_data):
+#         return Movie.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         # instance.title is the old title
+#         # validated_data.get('title', instance.title) is the new title
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.year = validated_data.get('year', instance.year)
+#         instance.save()
+#         return instance
+#
+#     # field level validation
+#     def validate_title(self, value):
+#         if len(value) < 2:
+#             raise serializers.ValidationError('Title is too short')
+#         return value
+#
+#     # object level validation
+#     def validate(self, data):
+#         if data['title'] == data['description']:
+#             raise serializers.ValidationError('Title and description must be different')
+#         return data
