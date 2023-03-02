b03d5d897a7d4fae929e284a0bff4ad257fb0b72
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2023-03-02 23:30:27 +0200

    Add more validators on serializers, Object validators, field validators, add validators by validator attr

--- watchlist/api/serializers.py
+++ watchlist/api/serializers.py
@@ -4,9 +4,17 @@ from rest_framework import serializers
 from watchlist.models import Movie
 
 
+# other types of validators
+def title_unique(value):
+    if Movie.objects.filter(title=value).exists():
+        raise serializers.ValidationError('Title must be unique')
+    return value
+
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=200)
+    title = serializers.CharField(max_length=200, validators=[title_unique]
+                                  )  # validators are used to check if the title is unique
     description = serializers.CharField()
     active = serializers.BooleanField(default=False)
     year = serializers.IntegerField()
@@ -23,3 +31,15 @@ class MovieSerializer(serializers.Serializer):
         instance.year = validated_data.get('year', instance.year)
         instance.save()
         return instance
+
+    # field level validation
+    def validate_title(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError('Title is too short')
+        return value
+
+    # object level validation
+    def validate(self, data):
+        if data['title'] == data['description']:
+            raise serializers.ValidationError('Title and description must be different')
+        return data
