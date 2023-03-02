3787a70e0fe2cbb1d458675032d2be7e0e43c602
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2023-03-02 02:30:58 +0200

    update urls to match with class base views

--- watchlist/api/urls.py
+++ watchlist/api/urls.py
@@ -1,12 +1,18 @@
 """URLs for the watchlist app."""
 from django.urls import path
 
-from watchlist.api.views import (
-    movie_list, movie_detail
+from watchlist.api.views import \
+    (
+    # movie_list, movie_detail,
+    MovieList, MovieDetail,
 )
 
 app_name = 'watchlist'
 urlpatterns = [
-    path('list/', movie_list, name='movie_list'),
-    path('<int:movie_id>', movie_detail, name='movie_detail'),
+    # function based views
+    # path('list/', movie_list, name='movie_list'),
+    # path('<int:movie_id>', movie_detail, name='movie_detail'),
+    # class based views
+    path('list/', MovieList.as_view(), name='movie_list'),
+    path('<int:movie_id>', MovieDetail.as_view(), name='movie_detail'),
 ]
