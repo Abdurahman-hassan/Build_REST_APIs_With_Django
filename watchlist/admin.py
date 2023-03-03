cca21dd3fb83635c35ebaaec4cf9ba9c9564f65b
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2023-03-03 03:13:50 +0200

    add StreamPlatform in admin

--- watchlist/admin.py
+++ watchlist/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import WatchMoviesList
+from .models import WatchMoviesList, StreamPlatform
 
 admin.site.register(WatchMoviesList)
+admin.site.register(StreamPlatform)
