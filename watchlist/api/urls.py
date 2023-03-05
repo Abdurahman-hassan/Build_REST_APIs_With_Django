21130352672704e9369097215989a86f62f9fc2d
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2023-03-06 01:46:49 +0200

    edit on urls by adding - instead of _

--- watchlist/api/urls.py
+++ watchlist/api/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     # path('list/', movie_list, name='movie_list'),
     # path('<int:movie_id>', movie_detail, name='movie_detail'),
     # class based views
-    path('list/', MovieList.as_view(), name='movie_list'),
-    path('<int:movie_id>', MovieDetail.as_view(), name='movie_detail'),
-    path('stream/', StreamPlatformList.as_view(), name='streamplatform_list'),
-    path('stream/<int:platform_id>/', StreamPlatformDetail.as_view(), name='streamplatform_detail'),
+    path('list/', MovieList.as_view(), name='movie-list'),
+    path('<int:movie_id>', MovieDetail.as_view(), name='movie-detail'),
+    path('stream/', StreamPlatformList.as_view(), name='streamplatform-list'),
+    path('stream/<int:platform_id>/', StreamPlatformDetail.as_view(), name='streamplatform-detail'),
 ]
