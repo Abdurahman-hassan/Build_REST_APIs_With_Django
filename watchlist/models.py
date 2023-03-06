55472d72390399fe95901014d5d4281586bc9971
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2023-03-06 02:10:34 +0200

    Add Review model

--- watchlist/models.py
+++ watchlist/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -26,3 +27,22 @@ class WatchMoviesList(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.year})"
+
+
+class Review(models.Model):
+    """A review for a movie."""
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1),
+                                                     MaxValueValidator(5)])
+    review = models.CharField(max_length=200, null=True)
+    active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # each review has one movie
+    # and each movie has many reviews
+    watchlist = models.ForeignKey(WatchMoviesList,
+                              on_delete=models.CASCADE,
+                              related_name='reviews')
+
+    def __str__(self):
+        return f"{self.watchlist.title} ({self.rating})"
