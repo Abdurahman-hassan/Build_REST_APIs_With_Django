04931646ca19328ffb2455789c76784ed294a2ad
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2023-03-06 08:34:43 +0200

    Add a router in urls

--- watchlist/api/urls.py
+++ watchlist/api/urls.py
@@ -1,13 +1,21 @@
 """URLs for the watchlist app."""
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from watchlist.api.views import \
     (
     # movie_list, movie_detail,
     MovieList, MovieDetail, StreamPlatformList, StreamPlatformDetail, ReviewList, ReviewDetail, ReviewCreate,
+    StreamPlatformVs,
 )
 
 app_name = 'watchlist'
+
+# routers help us to create urls for our models by combining the urls
+router = routers.DefaultRouter()
+# stream is the url name
+router.register('stream', StreamPlatformVs, basename='streamplatform')
+
 urlpatterns = [
     # function based views
     # path('list/', movie_list, name='movie_list'),
@@ -15,8 +23,12 @@ urlpatterns = [
     # class based views
     path('list/', MovieList.as_view(), name='movie-list'),
     path('<int:movie_id>', MovieDetail.as_view(), name='movie-detail'),
-    path('stream/', StreamPlatformList.as_view(), name='streamplatform-list'),
-    path('stream/<int:platform_id>/', StreamPlatformDetail.as_view(), name='streamplatform-detail'),
+
+    # we will comment this because we will use routers
+    # path('stream/', StreamPlatformList.as_view(), name='streamplatform-list'),
+    # path('stream/<int:platform_id>/', StreamPlatformDetail.as_view(), name='streamplatform-detail'),
+    path('', include(router.urls)),
+
 
     # generic class based views with relationships
     path('stream/<int:watchlist_id>/review-create/', ReviewCreate.as_view(), name='review-create'),
