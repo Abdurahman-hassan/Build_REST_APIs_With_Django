0069cd185900828445923b8c0c8ff0836b60b598
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2023-03-06 03:57:32 +0200

    Update the URLs by adding review, review/<int:pk>/

--- watchlist/api/urls.py
+++ watchlist/api/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from watchlist.api.views import \
     (
     # movie_list, movie_detail,
-    MovieList, MovieDetail, StreamPlatformList, StreamPlatformDetail,
+    MovieList, MovieDetail, StreamPlatformList, StreamPlatformDetail, ReviewList, ReviewDetail,
 )
 
 app_name = 'watchlist'
@@ -17,4 +17,8 @@ urlpatterns = [
     path('<int:movie_id>', MovieDetail.as_view(), name='movie-detail'),
     path('stream/', StreamPlatformList.as_view(), name='streamplatform-list'),
     path('stream/<int:platform_id>/', StreamPlatformDetail.as_view(), name='streamplatform-detail'),
+
+    # generic class based views
+    path('review/', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
 ]
