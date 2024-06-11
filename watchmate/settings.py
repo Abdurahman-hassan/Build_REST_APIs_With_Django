29ae7b89c2a28939c8210371120e04694d5e19f6
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-11 05:35:51 +0300

    switch db to MySQL

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'watchmate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MoviesDB',
+        'OPTIONS': {
+            'read_default_file': '/usr/local/etc/my.cnf',
+        },
     }
 }
 
