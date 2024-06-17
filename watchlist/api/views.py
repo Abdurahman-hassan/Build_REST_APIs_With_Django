83eaa7c07c7d83930b68af8a7743d97ac9a27ad3
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-17 09:46:13 +0300

    Add Api view class

--- watchlist/api/views.py
+++ watchlist/api/views.py
@@ -105,6 +105,56 @@ def movie_detail_using_serializer_class(request, movie_id):
 
 
 ############################################################################################################
+############################################################################################################
+# class based views
+############################################################################################################
+
+class MovieListAV(APIView):
+    """List all movies with ApiViewClass."""
+
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = ManualMovieSerializer(movies, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ManualMovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            # this serializer.data is the data that we just created of the return of the serializer created function
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MovieDetailAV(APIView):
+    """Retrieve, update or delete a movie instance."""
+
+    def get_object(self, pk):
+        try:
+            movie = Movie.objects.get(pk=pk)
+            return movie
+        except Movie.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        movie = self.get_object(pk)
+        serializer = ManualMovieSerializer(movie)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        movie = self.get_object(pk)
+        serializer = ManualMovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        movie = self.get_object(pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 
 # We will use the generic class based views itself
 # we don't need to define the get, post, put, delete methods
