19e5328765916d288e0c8ef9c59504d1400f3183
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-18 05:26:09 +0300

    add StreamPlatformSerializer

--- watchlist/api/serializers.py
+++ watchlist/api/serializers.py
@@ -104,3 +104,53 @@ class MovieSerializer(serializers.ModelSerializer):
         elif Movie.objects.filter(name=value).exists():
             raise serializers.ValidationError('Movie name already exists')
         return value
+
+
+class StreamPlatformSerializer(serializers.HyperlinkedModelSerializer):
+    """Serializer for the stream platform model."""
+    # watchlist is the name of related_name in the WatchList model
+    watchlist = WatchListSerializer(many=True, read_only=True)
+
+    # add string related field to get specific fields of the related objects not all of them
+    # watchlist = serializers.StringRelatedField(many=True, read_only=True)
+    # add primary key related field to get the primary key of the related objects
+    # watchlist = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    # add hyperlink related field to get the url of the related objects
+    # we mapped the watchlist to the watchlist-detail url
+    # we need to attach <appname>:<url-name> to the view_name
+    # because we are using the app_name = watchlist in the urls.py
+    # watchlist = serializers.HyperlinkedRelatedField(
+    #     many=True,
+    #     read_only=True,
+    #     view_name='watchlist:watchlist-detail'
+    # )
+
+    class Meta:
+        model = StreamPlatform
+        fields = "__all__"
+        # This is because we are using the HyperlinkedModelSerializer
+        # and we need to specify the view name and the lookup field
+        # we need to attach <appname>:<url-name> to the view_name
+        # because we are using the app_name = watchlist in the urls.py
+        # if we don't added the appname inside urls.y we can remove extra_kwargs
+        # because it's automatically added by the HyperlinkedModelSerializer
+        extra_kwargs = {
+            'url': {'view_name': 'watchlist:streamplatform-detail', 'lookup_field': 'pk'}
+        }
+
+    def validateـabout(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError('About platform is too long it should be less than 500 characters')
+        return value
+
+        # we can use serializers.Serializer to create a serializer
+        # other types of validators
+
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError('name is too short')
+
+        if StreamPlatform.objects.filter(name=value).exists():
+            raise serializers.ValidationError("name already exists")
+
+        return value
