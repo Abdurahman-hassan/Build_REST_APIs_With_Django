106fe5dc6a211fbb2ec8a6a3de99b8c9fb2d6d40
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-18 05:27:36 +0300

    fix linting, update the urls naming

--- watchlist/api/urls.py
+++ watchlist/api/urls.py
@@ -5,18 +5,18 @@ from rest_framework import routers
 from watchlist.api.views import \
     (
     # movie_list, movie_detail,
-    StreamPlatformList,
-    StreamPlatformDetail,
+    # StreamPlatformList,
+    # StreamPlatformDetail,
     ReviewList,
     ReviewDetail,
     ReviewCreate,
     StreamPlatformVs,
-    movie_list_manual_serializer_deserializer,
-    single_movie_manual_serializer_deserializer,
-    movie_detail_manual_serializer_deserializer,
-    movie_list_using_serializer_class,
-    single_movie_using_serializer_class,
-    movie_detail_using_serializer_class, MovieListAV, MovieDetailAV
+    watch_list_manual_serializer_deserializer,
+    single_watch_list_manual_serializer_deserializer,
+    watch_list_detail_manual_serializer_deserializer,
+    watch_list_using_serializer_class,
+    single_watch_list_using_serializer_class,
+    watch_list_detail_using_serializer_class, WatchListAV, WatchListDetailAV, StreamPlatformAV, StreamPlatformDetailAV
 )
 
 app_name = 'watchlist'
@@ -33,28 +33,25 @@ urlpatterns = [
     # function based views
     ##################################################################################
     # using manual serializer and deserializer
-    # path('fbv-movie-list', movie_list_manual_serializer_deserializer, name='fbv_watchlist'),
-    # path('fbv-movie-detail/<int:movie_id>', single_movie_manual_serializer_deserializer, name='fbv_movie_detail'),
-    # path('fbv-movie-more-detail/<int:movie_id>', movie_detail_manual_serializer_deserializer,
+    # path('fbv-watchlist-list', watch_list_manual_serializer_deserializer, name='fbv_watchlist'),
+    # path('fbv-watchlist-detail/<int:movie_id>', single_watch_list_manual_serializer_deserializer, name='fbv_movie_detail'),
+    # path('fbv-watchlist-more-detail/<int:movie_id>', watch_list_detail_manual_serializer_deserializer,
     #      name='fbv_movie_more_detail'),
     ##################################################################################
+    ##################################################################################
     # using a serializer class
-    path('fbv-movie-list', movie_list_using_serializer_class, name='fbv_watchlist'),
-    path('fbv-movie-detail/<int:movie_id>', single_movie_using_serializer_class, name='fbv_movie_detail'),
-    path('fbv-movie-more-detail/<int:movie_id>', movie_detail_using_serializer_class,
+    path('fbv-watchlist-list', watch_list_using_serializer_class, name='fbv_watchlist'),
+    path('fbv-watchlist-detail/<int:movie_id>', single_watch_list_using_serializer_class, name='fbv_movie_detail'),
+    path('fbv-watchlist-more-detail/<int:movie_id>', watch_list_detail_using_serializer_class,
          name='fbv_movie_more_detail'),
     ##################################################################################
     ##################################################################################
     # class based views
     ##################################################################################
-    path('cbv-movie-list', MovieListAV.as_view(), name='cbv_watchlist'),
-    path('cbv-movie-detail/<int:pk>', MovieDetailAV.as_view(), name='cbv_movie_detail'),
-    ##################################################################################
-
-
-    # we will comment this because we will use routers
-    # path('stream/', StreamPlatformList.as_view(), name='streamplatform-list'),
-    # path('stream/<int:platform_id>/', StreamPlatformDetail.as_view(), name='streamplatform-detail'),
+    path('list/', WatchListAV.as_view(), name='watchlist-list'),
+    path('list/<int:pk>/', WatchListDetailAV.as_view(), name='watchlist-detail'),
+    path('stream/', StreamPlatformAV.as_view(), name='streamplatform-list'),
+    path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
 
     # generic class based views with relationships
     path('stream/<int:watchlist_id>/review-create/', ReviewCreate.as_view(), name='review-create'),
