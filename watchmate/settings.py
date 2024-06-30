c00e5c1971d2d90e98f75bb36c88c8ea92b55706
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-30 10:35:28 +0300

    add defaults REST_FRAMEWORK settings

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -128,3 +128,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# REST Framework Settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
